0628e74f300bd96da86b3d4d05b291ddc21f5509
BobbySU <radio_20bg@yahoo.com>
2022-03-31 19:45:37 +0300

    Add All Java DB Spring Data Exam - 29 Mar 2020

--- Java DB Spring Data Exam - 29 Mar 2020/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
+++ Java DB Spring Data Exam - 29 Mar 2020/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
@@ -59,7 +59,18 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public String getCarsOrderByPicturesCountThenByMake() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        carRepository.findCarsOrderByPictureCountDescAndMake()
+                .forEach(car -> {
+                    sb.append(String.format("Car make - %s, model - %s\n" +
+                            "\tKilometers - %d\n" +
+                            "\tRegistered on - %s\n" +
+                            "\tNumber of pictures - %d\n",
+                            car.getMake(), car.getModel(), car.getKilometers(),
+                            car.getRegisteredOn(), car.getPictures().size()))
+                            .append(System.lineSeparator());
+                });
+        return sb.toString();
     }
 
     @Override
